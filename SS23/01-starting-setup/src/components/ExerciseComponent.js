cbb72a5ccd35ce3ad864dbd5a5026849a9e8e487
KobeMChamba <kchamba01@gmail.com>
2023-06-29 16:27:49 -0500

    Furthered React Udemy course

---
+++ SS23/01-starting-setup/src/components/ExerciseComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+// Todo: Add your component code
+// Important: Use export default function MyCmp() { ... }, don't define "export default" on a separate line!
+
+export default function ExerciseComponent() {
+    return <h1>First exercise - done! </h1>;
+}
