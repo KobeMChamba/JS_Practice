a5edfb2971042a7f412ff59b8bfd27c93856a8b9
KobeMChamba <kchamba01@gmail.com>
2023-06-30 14:10:30 -0500

    Completed up to video 51 of Udemy React course

--- SS23/01-starting-setup/src/components/ExpenseItem.js
+++ SS23/01-starting-setup/src/components/ExpenseItem.js
@@ -1,18 +1,18 @@
 import './ExpenseItem.css';
+import Card from "./Card";
+import ExpenseDate from './ExpenseDate';
 //Component in react is just a js function
 //Using destrcuring
 //function ExpenseItem({date, title, price}) {
 function ExpenseItem(props) {
-
-
   return (
-    <div className="expense-item">
-      
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
